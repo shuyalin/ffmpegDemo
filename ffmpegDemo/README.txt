724a2e0b3e3669829b707bfd503fabce56dc8468
shuyalin <389147942@qq.com>
2021-09-21 23:33:15 +0800

    更改所有文件的权限为读写执行权限

---
+++ ffmpegDemo/README.txt
@@ -0,0 +1,1 @@
+gcc test_one.c -I ../include/ -L ../lib/ -lavcodec
---
+++ ffmpegDemo/test_one.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <libavcodec/avcodec.h>
+int main()
+{
+	printf("ffffffffffffff %s\n",avcodec_configuration());
+	return 0;
+}
